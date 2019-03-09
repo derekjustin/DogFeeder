f564b85c73eb5852020ee57d44153dafbeff9353
Justin Strelka <jstrelka@student.cccs.edu>
2019-03-09 12:05:19 -0700

    created dogFeederDriver class and began building user interface

---
+++ dogFeederDriver.py
@@ -0,0 +1,12 @@
+def main():
+
+    objective = ""
+    quit = True
+
+    # Greeting
+    print("Hello welcome to DogFeeder")
+
+    # while loop to continue program
+    while quit:
+
+        objective = input("What would you like to do?")
