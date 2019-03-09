af8482ae7a356b1b201a0d61031dd64e36b267a4
Justin Strelka <jstrelka@student.cccs.edu>
2019-03-09 11:53:31 -0700

    foodClass member variables / setters and getters defined

---
+++ foodClass.py
@@ -0,0 +1,13 @@
+def foodClass():
+
+    #foodClass Constructor
+    def __init__(self, calsPerCupIn):
+        self.calsPerCup = calsPerCupIn
+
+    #Member Variable Setters
+    def setCalsPerCup(self, calsPerCupIn):
+        self.calsPerCup = calsPerCupIn
+
+    #Member Variable Getters
+    def getCalsPerCup(self):
+        return self.calsPerCup
