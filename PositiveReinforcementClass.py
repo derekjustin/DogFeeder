85d743b3bbbc6ea2c6288355be704169a8b7b403
Justin Strelka <jstrelka@student.cccs.edu>
2019-03-10 18:58:20 -0600

    cleaned up PositiveReinforcementClass a bit and added variable setters and getters

--- PositiveReinforcementClass.py
+++ PositiveReinforcementClass.py
@@ -1,14 +1,32 @@
 import random
 
-def PositiveReinforcementClass():
+class PositiveReinforcementClass:
+
+    def __init__(self):
+        self.feed_1 = 0
+        self.feed_2 = 0
+
+    # Member variable setters
+    def setFeed_1(self, feedIn):
+        self.feed_1 = feedIn
+
+    def setFeed_2(self, feedIn):
+        self.feed_2 = feedIn
+
+    # Member variable getters
+    def getFeed_1(self):
+        return self.feed_1
+
+    def getFeed_2(self):
+        return self.feed_2
 
     def variedFeeding1(self):
-        feed_1 = random.randrange(0, self.cupsPerDay)      
-        return feed_1
+        self.feed_1 = random.randrange(0, self.cupsPerDay)
+        return self.feed_1
 
 #Problems, variedFeeding1 must happen before variedFeeding2
     def variedFeeding2(self):
-        feed_2 = (self.maxCaloricIntake() - self.variedFeeding1) 
-        return feed_2
+        self.feed_2 = (self.maxCaloricIntake() - self.variedFeeding1)
+        return self.feed_2
 
 
