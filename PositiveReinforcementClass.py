f1a968a33b4cc5c63737ca977cb528fd73e11b57
Justin Strelka <jstrelka@student.cccs.edu>
2019-03-11 11:27:11 -0600

    added positiveReinforcement attributes to dogClass

--- PositiveReinforcementClass.py
+++ PositiveReinforcementClass.py
@@ -3,30 +3,30 @@ import random
 class PositiveReinforcementClass:
 
     def __init__(self):
-        self.feed_1 = 0
-        self.feed_2 = 0
+        self._feed_1 = 0
+        self._feed_2 = 0
 
     # Member variable setters
     def setFeed_1(self, feedIn):
-        self.feed_1 = feedIn
+        self._feed_1 = feedIn
 
     def setFeed_2(self, feedIn):
-        self.feed_2 = feedIn
+        self._feed_2 = feedIn
 
     # Member variable getters
     def getFeed_1(self):
-        return self.feed_1
+        return self._feed_1
 
     def getFeed_2(self):
-        return self.feed_2
+        return self._feed_2
 
     def variedFeeding1(self):
-        self.feed_1 = random.randrange(0, self.cupsPerDay)
-        return self.feed_1
+        self._feed_1 = random.randrange(0, self.cupsPerDay)
+        return self._feed_1
 
 #Problems, variedFeeding1 must happen before variedFeeding2
     def variedFeeding2(self):
-        self.feed_2 = (self.maxCaloricIntake() - self.variedFeeding1)
-        return self.feed_2
+        self._feed_2 = (self.maxCaloricIntake() - self.variedFeeding1)
+        return self._feed_2
 
 
--- dogClass.py
+++ dogClass.py
@@ -1,4 +1,4 @@
-
+from PositiveReinforcementClass import *
 
 class dogClass:
 
@@ -7,24 +7,8 @@ class dogClass:
         self._weight = 0
         self._age = 0
         self._name = ""
+        self._positiveReinforcement = PositiveReinforcementClass()
 
-    def __gt__(self, anotherPet):
-        return str(self._name).upper() > anotherPet._name.upper()
-
-    def __ge__(self, anotherPet):
-        return str(self._name).upper() >= anotherPet._name.upper()
-
-    def __lt__(self, anotherPet):
-        return str(self._name).upper() < anotherPet._name.upper()
-
-    def __le__(self, anotherPet):
-        return str(self._name).upper() <= anotherPet._name.upper()
-
-    def __str__(self):
-        return "Pet Name: " + str(self._name) + "\nPet Age: " + str(self._age) + "\nPet Weight: " + str(self._weight)
-
-    def toFile(self):
-        return str(self._name), str(self._age), str(self._weight)
     # Member Variable Setters
     def setName(self, nameIn):
         self._name = nameIn
@@ -47,7 +31,25 @@ class dogClass:
         return self._weight
 
 
+    # Operator Overloads
+    def __gt__(self, anotherPet):
+        return str(self._name).upper() > anotherPet._name.upper()
 
+    def __ge__(self, anotherPet):
+        return str(self._name).upper() >= anotherPet._name.upper()
 
+    def __lt__(self, anotherPet):
+        return str(self._name).upper() < anotherPet._name.upper()
+
+    def __le__(self, anotherPet):
+        return str(self._name).upper() <= anotherPet._name.upper()
+
+    def __str__(self):
+        return "Pet Name: " + str(self._name) + "\nPet Age: " + str(self._age) + "\nPet Weight: " + str(self._weight)
+
+    # File formatting functions
+    def toFile(self):                                             # Add attributes when positiveReinforcement complete
+        return str(self._name), str(self._age), str(self._weight) #, str(self._positiveReinforcement.getFeed_1()),\
+                                                                  # str(self._positiveReinforcement.getFeed_2())
 
 
